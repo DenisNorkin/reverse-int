ca1909f09efb23976027b131ee50a55a50931b00
Denis.Norkin <denis.norkin.2019@gmail.com>
2022-01-04 09:58:08 +0500

    feat: add a solution to the problem index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = '';
+  if (Math.sign(n) === -1) {
+    str = -n + '';
+  } else {
+    str = n + '';
+  }
+  return +str.split('').reverse().join('');
 }
